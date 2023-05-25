b67693178c240550e5dfad5dfbce861e09eeb781
Morten Stephansen <morten-ks@hotmail.com>
2023-05-25 10:56:05 +0200

    TCP-server done, no ROS

--- TCP_server/PLC_mimic_client.py
+++ TCP_server/PLC_mimic_client.py
@@ -7,7 +7,7 @@ PORT = 20001  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'<carrierInformation><stationID>ST_PLC_13</stationID><carrierID>14</carrierID><timeDate>20/04/2023: 08:26:29</timeDate></carrierInformation>')
+    s.sendall(b'<carrierInformation><stationID>ST_PLC_11</stationID><carrierID>14</carrierID><timeDate>20/04/2023: 08:26:29</timeDate></carrierInformation>')
     data = s.recv(16384)
 
 print(f"Received {data!r}")
